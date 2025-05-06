a50fed79ef942804d3491ac5ddd769e968fd8310
termdisc <outphase@gmail.com>
2025-05-06 10:56:18 -0500

    Delete umu-504230.py

--- gamefixes-egs/umu-504230.py
+++
@@ -1,9 +0,0 @@
-"""Celeste"""
-# EGS-ID Salt
-
-from protonfixes import util
-
-
-def main() -> None:
-    util.set_environment('FNA3D_FORCE_DRIVER','OpenGL')
-    # Vulkan is broken in the EGS build
